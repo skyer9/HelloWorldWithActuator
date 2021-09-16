03f5190e31cbd834627ccb31dda36b1b3bfaf864
skyer9 <skyer9@gmail.com>
2021-09-16 09:50:43 +0900

    Initial commit

---
+++ src/main/java/com/example/helloworldwithactuator/HelloWorldWithActuatorApplication.java
@@ -0,0 +1,20 @@
+package com.example.helloworldwithactuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HelloWorldWithActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldWithActuatorApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String hello() {
+		return "Hello, World!";
+	}
+}
---
+++ src/test/java/com/example/helloworldwithactuator/HelloWorldWithActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helloworldwithactuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldWithActuatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
